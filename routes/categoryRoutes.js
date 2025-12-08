7141e7a148583777f6d06f5e114dc0956fbf41aa
Luis <luisangelhc2004@hotmail.com>
2025-12-07 18:17:04 -0600

    Se arregla problema de rutas

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { 
-    getAllCategories, 
-    getCategoryById,
-    getCategoryByName } from "../controllers/categoryController.js";
+  getAllCategories, 
+  getCategoryById,
+  getCategoryByName,
+  createCategory      // 👈 NUEVO
+} from "../controllers/categoryController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";    // 👈 NUEVO
+import { verifyAdminOnly } from "../middlewares/roleMiddleware.js"; // 👈 NUEVO
 
 const router = express.Router();
 
+// Rutas públicas
 router.get("/", getAllCategories);
 router.get("/:id", getCategoryById);
 router.get("/name/:categoryName", getCategoryByName);
+
+// Rutas sólo admin
+router.post("/", verifyToken, verifyAdminOnly, createCategory);   // 👈 NUEVA
 
 export default router;
