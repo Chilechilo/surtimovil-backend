dfb9eff1c4340835a10ae753438b996b0a2f40e0
Luis <luisangelhc2004@hotmail.com>
2025-12-08 16:20:53 -0600

    Se corrige error de pedido 3

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
@@ -13,12 +14,18 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
+app.use((req, res, next) => {
+  console.log("📨 REQUEST:", req.method, req.originalUrl);
+  console.log("📦 BODY RECIBIDO:", req.body);
+  next();
+});
+
+// Conexión DB
 connectDB();
 
-// Registrar rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
@@ -26,4 +33,4 @@ app.use("/api/reports", reportRoutes);
 app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
